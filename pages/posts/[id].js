c676ff720c595e7b230e81862e2405f8c500c374
a-know <a.know.dev@gmail.com>
2020-04-27 22:00:59 +0900

    add title

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -21,6 +21,9 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
